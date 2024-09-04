9c1580e13da66e835f66f5922680daea7fe1b348
Randy Kangas <randy.kangas@yardi.com>
2024-09-04 11:15:38 -0600

    parameterized queries

--- flask_webgoat/auth.py
+++ flask_webgoat/auth.py
@@ -8,20 +8,16 @@ bp = Blueprint("auth", __name__)
 def login():
     username = request.form.get("username")
     password = request.form.get("password")
-    if username is None or password is None:
-        return (
-            jsonify({"error": "username and password parameter have to be provided"}),
-            400,
-        )
+    if not username or not password:
+        return jsonify({"error": "username and password parameter have to be provided"}), 400
 
-    # vulnerability: SQL Injection
-    query = (
-        "SELECT id, username, access_level FROM user WHERE username = '%s' AND password = '%s'"
-        % (username, password)
-    )
-    result = query_db(query, [], True)
+    # Use parameterized query
+    query = "SELECT id, username, access_level FROM user WHERE username = ? AND password = ?"
+    result = query_db(query, (username, password), True)
+
     if result is None:
         return jsonify({"bad_login": True}), 400
+
     session["user_info"] = (result[0], result[1], result[2])
     return jsonify({"success": True})
 
